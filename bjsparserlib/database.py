2a24986a5f27782e060ab18c5272daebf7e090ba
Austin <metalnut4@netscape.net>
2019-12-30 16:08:57 +0000

    Structuring as a library.
    This will allow it to more easily be utilized by other Python packages.

--- bjsparserlib/database.py
+++ bjsparserlib/database.py
@@ -1,8 +1,8 @@
+import argparse
 import collections
 import json
 import os
 import sqlite3
-import sys
 
 EXCLUDE_CATEGORIES = [
     "Apparel",
@@ -46,11 +46,15 @@ EXCLUDE_CATEGORIES = [
     ["Home", "Rugs & Flooring"]
 ]
 
-def load_inventory():
-    with open("products.json") as bjs_items_file:
-        return json.load(bjs_items_file)
+def _load_inventory(inventory_filepath):
+    with open(inventory_filepath) as inventory_file:
+        return json.load(inventory_file)
 
-def _insert_if_new(table_name, data):
+def _db_disconnect(cursor, connection):
+    connection.commit()
+    connection.close()
+
+def _insert_if_new(cursor, table_name, data):
     colnames = list(data.keys())
     coldata = [data[colname] for colname in colnames]
 
@@ -70,46 +74,66 @@ def _insert_if_new(table_name, data):
     else:
         raise Exception("There are multiple rows matching {}".format(data))
 
-def insert_categories(categories, store):
+def _insert_categories(cursor, categories, store):
     if categories:
         category = max(categories, key=len)
     else:
         return None
 
-    parent_id = _insert_if_new("categories", {"name": category[0], "store": store})
+    parent_id = _insert_if_new(cursor, "categories", {"name": category[0], "store": store})
     for subcategory in category[1:]:
-        parent_id = _insert_if_new("categories", {"name": subcategory, "store": store, "parentid": parent_id})
+        parent_id = _insert_if_new(cursor, "categories", {"name": subcategory, "store": store, "parentid": parent_id})
     return parent_id
 
-def exclude(categories, exclude_categories):
+def _exclude(categories, exclude_categories):
     for category in categories:
         for index in range(len(category)):
             if tuple(category[:index + 1]) in exclude_categories:
                 return True
     return False
 
-db_path = sys.argv[1] if len(sys.argv) >= 2 else "inventory.db"
-connection = sqlite3.connect(db_path)
-cursor = connection.cursor()
+def _insert_inventory(cursor, inventory):
+    exclude_categories = {(category, ) if isinstance(category, str) else tuple(category) for category in EXCLUDE_CATEGORIES}
 
-cursor.execute("PRAGMA foreign_keys = ON")
-cursor.execute("CREATE TABLE IF NOT EXISTS categories (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, store TEXT, parentid INTEGER, FOREIGN KEY(parentid) REFERENCES categories(id))")
-cursor.execute("""CREATE TABLE IF NOT EXISTS products (name, categoryid, url, store, stocked)""")
+    store = "BJs"
+    for item in inventory["inventory"]:
+        if _exclude(item["categories"], exclude_categories):
+            continue
 
-exclude_categories = {(category, ) if isinstance(category, str) else tuple(category) for category in EXCLUDE_CATEGORIES}
+        category_id = _insert_categories(cursor, item["categories"], store)
+        if not category_id:
+            print("No category information for {}".format(item["name"]))
+            continue
 
-store = "BJs"
-inventory = load_inventory()
-for item in inventory["items"]:
-    if exclude(item["categories"], exclude_categories):
-        continue
+        cursor.execute("INSERT INTO products (name, categoryid, url, store, stocked) VALUES (?, ?, ?, ?, ?)", (item["name"], category_id, item["url"], store, None))
 
-    category_id = insert_categories(item["categories"], store)
-    if not category_id:
-        print("No category information for {}".format(item["name"]))
-        continue
+def _create_tables(cursor):
+    cursor.execute("PRAGMA foreign_keys = ON")
+    cursor.execute("CREATE TABLE IF NOT EXISTS categories (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, store TEXT, parentid INTEGER, FOREIGN KEY(parentid) REFERENCES categories(id))")
+    cursor.execute("CREATE TABLE IF NOT EXISTS products (name, categoryid, url, store, stocked)")
 
-    cursor.execute("INSERT INTO products (name, categoryid, url, store, stocked) VALUES (?, ?, ?, ?, ?)", (item["name"], category_id, item["url"], store, None))
+def _db_connect(database_filepath):
+    connection = sqlite3.connect(database_filepath)
+    cursor = connection.cursor()
+    return connection, cursor
 
-connection.commit()
-connection.close()
+def populate(inventory, database_filepath):
+    connection, cursor = _db_connect(database_filepath)
+
+    _create_tables(cursor)
+    _insert_inventory(cursor, inventory)
+
+    _db_disconnect(cursor, connection)
+
+def populate_from_file(inventory_filepath, database_filepath):
+    populate(_load_inventory(inventory_filepath), database_filepath)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("database_filepath")
+    parser.add_argument("inventory_filepath")
+    return vars(parser.parse_args())
+
+if __name__ == "__main__":
+    args = parse_args()
+    populate_from_file(args["inventory_filepath"], args["database_filepath"])
