ce34a857c353e53c5de28407ab0b0049282c5daa
Austin <mathfreak65@gmail.com>
2020-02-19 13:02:13 +0000

    Remove database output.

--- bjsparserlib/__init__.py
+++ bjsparserlib/__init__.py
@@ -1,6 +1,1 @@
 from bjsparserlib.inventory import download
-from bjsparserlib.database import populate
-
-def download_and_populate(inventory_filepath, database_filepath):
-    inventory = download(inventory_filepath)
-    populate(inventory, database_filepath)
